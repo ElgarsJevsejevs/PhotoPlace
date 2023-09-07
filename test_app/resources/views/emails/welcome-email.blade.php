e1e9279e825ed4fbfc8b7ad34e5fd4f2295b82fc
ElgarsJevsejevs <elgarsjevsejevs@gmail.com>
2023-09-07 12:31:53 +0300

    completed commit

---
+++ test_app/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+<x-mail::message>
+# Welcome to PhotoPlace!
+
+This is a community of a people that love photos and we love
+that you have joined us!
+
+All the best,<br>
+PhotoPlace
+</x-mail::message>
